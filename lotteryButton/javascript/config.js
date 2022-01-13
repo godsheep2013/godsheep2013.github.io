f432a147b16c0974524dbf5673037997495a30b9
godsheep2013 <godsheep2013@gmail.com>
2022-01-13 15:21:07 +0800

    upload js

---
+++ lotteryButton/javascript/config.js
@@ -0,0 +1,26 @@
+﻿var info = {
+	act_name: "中華電信110年年終感恩線上摸彩活動",
+	output_filename: "cht_result0110.csv",
+	can_back_if_not_appear: true,
+	restore_key: "CHT110年終摸彩活動0110"
+}
+
+var msg_conf = {
+	is_testing: false,
+	admin_sn: "864446",
+	user_id: "",
+	password: "",
+	post_text: "領獎時間及地點請依行政暨資產發展處E-mail通知，感謝您的配合。祝您虎虎生風虎年行大運",
+	url: "http://localhost:24680/javascript/msg.cht.js"
+}
+
+var firebase_conf = {
+	email: "chtnparty2015@gmail.com",
+	apiKey: "AIzaSyB-v_a3D0f4qDRwhxNTYRIMIpLVFaeGhD0",
+	authDomain: "resplendent-inferno-9545.firebaseapp.com",
+	databaseURL: "https://resplendent-inferno-9545.firebaseio.com",
+	storageBucket: "resplendent-inferno-9545.appspot.com",
+	messagingSenderId: "145676928228"
+}
+
+var justfont_conf = "<link href='//fonts.googleapis.com/css?family=Asap:700' rel='stylesheet' type='text/css'><script src=\"//s3-ap-northeast-1.amazonaws.com/justfont-user-script/jf-37614.js\"></script>    //<link href='//fonts.googleapis.com/css?family=Asap:700' rel='stylesheet' type='text/css'><script src=\"//s3-ap-northeast-1.amazonaws.com/justfont-user-script/jf-1234.js\"></script>";
